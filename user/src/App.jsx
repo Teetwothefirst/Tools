0c499a58b085fad97dbb2cd6f0973ea7fd4dec11
Teetwothefirst <dexterdavid835@gmail.com>
2025-03-13 08:21:58 -0700

    Admin and User Init - user startup (Login Screen)

---
+++ user/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Login from './components/Login'
+
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        {/* <div>Hey Hi</div> */}
+        <Login></Login>
+      </div>
+    </>
+  )
+}
+
+export default App
